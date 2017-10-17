0f5915666f146ed3025bc8390ce06f7e25b6eab2
Maria <marianaumenko@mail.ru>
2017-10-17 16:01:54 +0300

    Version1

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,16 @@
-// Your code here...
+less = {
+    env: "development",
+    async: false,
+    fileAsync: false,
+    poll: 1000,
+    functions: {},
+    dumpLineNumbers: "comments",
+    relativeUrls: false,
+    rootpath: ":/a.com/"
+};
+
+// Функция замены цвета товара
+function changeImg(lin){
+    var img = document.getElementById("img__id");
+    img.src=lin;
+};
