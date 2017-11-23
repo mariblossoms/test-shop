74947daaab0ec81296d4a985b33e56a9fe45dc40
Maria <marianaumenko@mail.ru>
2017-11-23 12:00:06 +0300

    Frame the last selection

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,57 +1,60 @@
 import PropertySelector from './property-selector.js';
 
 const productForm = document.querySelector('.js-info');
-let selectedTd;
-let selectedColor;
 const img = document.getElementById('productImg');
 const Dispatcher = document.getElementById('doc');
 
-new PropertySelector(document.getElementById('colorList'));
+new PropertySelector(document.getElementById('jsColor'));
+new PropertySelector(document.getElementById('jsSize'));
 
-
-Dispatcher.addEventListener('property-selected',ev => {
+Dispatcher.addEventListener('property-selected', ev => {
     const data = ev.detail;
     if (data.type === 'color') {
         localStorage.setItem(data.type, data.value);
         changePicture(data.value);
-    } else if (data.type === 'size') {
+    }
+    if (data.type === 'size') {
         localStorage.setItem(data.type, data.value);
+        changeSize(data.value);
     }
 });
 
 function changePicture(color) {
     img.src = 'img/tshirt_' + color + '.jpg';
+
+    const colorList = productForm.querySelectorAll('[data-value]');
+    for (let i = 0; i < colorList.length; i++) {
+        const selectedColor = colorList[i];
+        if (selectedColor.getAttribute('data-value') === color) {
+            selectedColor.classList.add('js-color_active');
+        } else {
+            selectedColor.classList.remove('js-color_active');
+        }
+    }
 }
 
-productForm.addEventListener('click', function (event) {
-    const el = event.target;
-    const classes = el.classList;
-    if (classes.contains('js-sizeProduct')) {
-        highlightSize(el);
-    } else if (classes.contains('js-color')) {
-        highlightColor(el);
+function changeSize(size) {
+    const sizeList = productForm.querySelectorAll('[data-value]');
+    for (let i = 0; i < sizeList.length; i++) {
+        const selectedSize = sizeList[i];
 
+        if (selectedSize.getAttribute('data-value') === size) {
+            selectedSize.classList.add('js-sizeProduct_active');
+        } else {
+            selectedSize.classList.remove('js-sizeProduct_active');
+        }
     }
-
-});
-
-function highlightSize(node) {
-    if(selectedTd) {
-        selectedTd.classList.remove('js-sizeProduct_active');
-    }
-    selectedTd = node;
-    selectedTd.classList.add('js-sizeProduct_active');
-}
-function highlightColor(node) {
-    if(selectedColor) {
-        selectedColor.classList.remove('js-color_active');
-    }
-    selectedColor = node;
-    selectedColor.classList.add('js-color_active');
 }
 
 const lastColor = localStorage.getItem('color');
-if (lastColor){
+const lastSize = localStorage.getItem('size');
+if (lastColor) {
     changePicture(lastColor);
-//    highlightColor(lastColor);
+    alert('Последний раз Вы выбрали - ' + lastColor + ' цвет');
 }
+if (lastSize) {
+    changeSize(lastSize);
+    alert('Последний раз Вы выбрали - ' + lastSize + ' размер');
+}
+
+
